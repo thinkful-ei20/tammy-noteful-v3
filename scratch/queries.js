6be0cf8f1a96782d5adf13ed7b0a134c70f102b4
Tammy Tian <tiantammy@outlook.com>
2018-05-02 03:11:07 +0800

    testing db

---
+++ scratch/queries.js
@@ -0,0 +1,34 @@
+'use strict';
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    const searchTerm = 'lady gaga';
+    let filter = {};
+
+    if (searchTerm) {
+      const re = new RegExp(searchTerm, 'i');
+      filter.title = { $regex: re };
+    }
+
+    return Note.find(filter)
+      .sort('created')
+      .then(results => {
+        console.log(results);
+      })
+      .catch(console.error);
+  })
+  .then(() => {
+    return mongoose.disconnect()
+      .then(() => {
+        console.info('Disconnected');
+      });
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
