517bb25a1f318e1b918eee08627aee019d409467
Tammy Tian <tiantammy@outlook.com>
2018-05-02 02:36:01 +0800

    added note schema in note.js

---
+++ models/note.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  createdAt: {type: Date, default: Date.now},
+  updatedAt: {type: Date, default:Date.now}
+});
+
+const Note = mongoose.model('Note',noteSchema);
+
+module.exports = {Note};
