eac2c48c9852acc507e3878842c3089ff0d13417
Tammy Tian <tiantammy@outlook.com>
2018-05-02 23:36:50 +0800

    added test env to config

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 
 exports.PORT = process.env.PORT || 8080;
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
