7cd013cfb7fa09f56542ca0cf3b52a31a5e0f603
Tammy Tian <tiantammy@outlook.com>
2018-05-04 02:22:57 +0800

    fixed typo to debug folderId

--- models/folder.js
+++ models/folder.js
@@ -2,10 +2,18 @@
 
 const mongoose = require('mongoose');
 
-const folderSchema = mongoose.Schema({ 
-  name:  {type: String, required: true}
+const folderSchema = new mongoose.Schema({ 
+  name:  {type: String, unique: true}
 }, {timestamps: true});
 
 const Folder = mongoose.model('Folder', folderSchema);
+
+folderSchema.set('toObject', {
+  transform: function(doc, ret){
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
 
 module.exports = {Folder};
