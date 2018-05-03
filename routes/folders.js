97dea3e2089baa35fbb7d042eaed5c8646e049bd
Tammy Tian <tiantammy@outlook.com>
2018-05-04 03:13:50 +0800

    added GET all

--- routes/folders.js
+++ routes/folders.js
@@ -1,14 +1,28 @@
 'use strict';
 
 const express = require('express');
-const router = express.router;
+const router = express.Router();
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const {MONGODB_URI} = require('../config');
+// const {MONGODB_URI} = require('../config');
 const {Folder} = require('../models/folder');
 
+//get all folders and search
+router.get('/', (req, res, next) => {
+  return Folder.find()
+    .sort({name: 1})
+    .then ((result) => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
+router.get('/:id', (req, res, next) => {
+
+});
 
 module.exports = router;
