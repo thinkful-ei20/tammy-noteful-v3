213f331e59e7472784a3ccabede93a5b2f229818
Tammy Tian <tiantammy@outlook.com>
2018-05-04 23:56:02 +0800

    added GET and GET by id routes

--- routes/tags.js
+++ routes/tags.js
@@ -10,5 +10,32 @@ const {MONGODB_URI} = require('../config');
 const {Note} = require('../models/note');
 const {Tag} = require('../models/tag');
 
+// GET/READ a single item
+router.get('/', (req, res, next) => {
+  return Tag
+    .find()
+    .sort({name: 1})
+    .then(results => {
+      res.json(results);
+    })
+    .catch (err => next (err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error ('Invalid Id');
+    err.status = 400;
+    return next(err);
+  }
+
+  return Tag
+    .findById(id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => next (err));
+});
 
 module.exports = router;
