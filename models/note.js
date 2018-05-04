fafdf228f05904cd02128058ecb5c8400819c7b3
Tammy Tian <tiantammy@outlook.com>
2018-05-04 23:20:52 +0800

    upadted note model to include tag

--- models/note.js
+++ models/note.js
@@ -7,6 +7,7 @@ const noteSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String},
   folderId: {type: mongoose.Schema.Types.ObjectId, ref: 'Folder'},
+  tags: [{type: mongoose.Schema.Types.ObjectId, ref:'Tag'}],
 }, {timestamps: true});
 
 noteSchema.set('toObject',  {
