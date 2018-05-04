4d1ad104bf429c242d567e9297749a81a5afc8ab
Tammy Tian <tiantammy@outlook.com>
2018-05-04 23:24:14 +0800

    updated seed-database to include tags

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 const {Note} = require('../models/note');
 const {Folder} = require('../models/folder');
+const {Tag} = require('../models/tag');
 
 const seedNotes = require('../db/seed/notes');
 const seedFolders = require('../db/seed/folders');
+const seedTags = require('../db/seed/tags');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
@@ -16,8 +18,9 @@ mongoose.connect(MONGODB_URI)
       Note.insertMany(seedNotes),
       Folder.insertMany(seedFolders),
       Folder.createIndexes(),
+      Tag.insertMany(seedTags),
+      Tag.createIndexes(),
     ]);
-      
   })
   .then(results => {
     console.log(results);
